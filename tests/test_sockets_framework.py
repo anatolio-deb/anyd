e3c6c203c29d5c3ab28632e6844eae4eb9c79d57
Anatolio Nikiforidis <nikiforova693@gmail.com>
2021-03-09 18:37:11 +0500

    edit: tests

--- tests/test_sockets_framework.py
+++ tests/test_sockets_framework.py
@@ -5,6 +5,7 @@ from tests import core as test_core
 
 test_address = ("localhost", 3000)
 test_server = Server(test_address, test_core)
+error = None
 
 
 def test_version():
@@ -16,38 +17,56 @@ def test_server_is_alive():
     test_server_process = Process(target=test_server.start)
     test_server_process.start()
     assert test_server_process.is_alive() is True
-    test_server_process.kill()
+    test_server_process.terminate()
 
 
 def test_normal_request():
-    global test_server
+    global test_server, error
     test_server_process = Process(target=test_server.start)
     test_server_process.start()
     with Client(test_address) as call:
         response = call.commit("test_function", "echo")
-    assert response == "echo"
-    test_server_process.kill()
+    try:
+        assert response == "echo"
+    except AssertionError as e:
+        error = e
+    finally:
+        test_server_process.terminate()
+        if isinstance(error, AssertionError):
+            raise error
 
 
 def test_not_implemented_function():
-    global test_server
+    global test_server, error
     test_server_process = Process(target=test_server.start)
     test_server_process.start()
     try:
         with Client(test_address) as call:
             call.commit("unknown_function", "echo")
     except NotImplementedError as e:
-        assert isinstance(e, NotImplementedError)
-    test_server_process.kill()
+        try:
+            assert isinstance(e, NotImplementedError)
+        except AssertionError as e:
+            error = e
+    finally:
+        test_server_process.terminate()
+        if isinstance(error, AssertionError):
+            raise error
 
 
 def test_multiple_requests():
-    global test_server
+    global test_server, error
     test_server_process = Process(target=test_server.start)
     test_server_process.start()
     with Client(test_address) as call:
-        response = call.commit("test_function", "echo_one")
-        assert response == "echo_one"
-        response = call.commit("test_function", "echo_two")
-        assert response == "echo_two"
-    test_server_process.kill()
+        response_one = call.commit("test_function", "echo_one")
+        response_two = call.commit("test_function", "echo_two")
+    try:
+        assert response_one == "echo_one"
+        assert response_two == "echo_two"
+    except AssertionError as e:
+        error = e
+    finally:
+        test_server_process.terminate()
+        if isinstance(error, AssertionError):
+            raise error
